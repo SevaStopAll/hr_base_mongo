39035eb13e9288748642c62f3bc1af51080ef571
SevaStopAll <sevastopallrh@yandex.ru>
2023-06-26 10:27:02 +0400

    Init comment

---
+++ src/main/java/ru/sevastopall/hrbase/HrbaseApplication.java
@@ -0,0 +1,13 @@
+package ru.sevastopall.hrbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrbaseApplication.class, args);
+	}
+
+}
