c283113b74df61f56ea2cdee51e71fbd16890e37
SevaStopAll <sevastopallrh@yandex.ru>
2023-06-27 11:40:54 +0400

    Add README.md

--- src/main/java/ru/sevastopall/hrbase/controller/CandidateController.java
+++ src/main/java/ru/sevastopall/hrbase/controller/CandidateController.java
@@ -27,43 +27,42 @@ public class CandidateController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> redirect(@PathVariable String id) {
+    public ResponseEntity<String> getById(@PathVariable String id) {
         Optional<Candidate> optional = candidateService.findById(id);
         if (optional.isEmpty()) {
             return ResponseEntity
                     .status(HttpStatus.NOT_FOUND).build();
         }
         var body =  optional.get().toString();
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .header("Status", "Found")
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(body);
     }
 
     @GetMapping("/findBySkill")
     public ResponseEntity<String> findSkill(@RequestParam(value = "skill", required = true) String skill) {
         var body = candidateService.findBySkillsContaining(skill).toString();
-        return ResponseEntity.status(HttpStatus.FOUND)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(body);
     }
 
     @GetMapping("/findByContact")
     public ResponseEntity<String> findByContact(@RequestParam(value = "contact", required = true) String contact) {
         var body = candidateService.findByContactsContaining(contact).toString();
-        return ResponseEntity.status(HttpStatus.FOUND)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(body);
     }
 
     @GetMapping("/filterAge")
     public ResponseEntity<String> filterByAge(@RequestParam(value = "from") int from, @RequestParam(value = "to") int to) {
         var body = candidateService.findCandidateByAgeBetween(from, to).toString();
-        return ResponseEntity.status(HttpStatus.FOUND)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(body);
     }
 
     @GetMapping("/filterCV")
     public ResponseEntity<String> filterByCV() {
         var body = candidateService.findCandidateByCV().toString();
-        return ResponseEntity.status(HttpStatus.FOUND)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(body);
     }
 
